79e1963374b920970e67ed332bccbcc8fa337eae
Abhishek Mukherjee <mukherjeea2017@gmail.com>
2022-01-05 02:11:39 +0530

    All files of assessment task

---
+++ index.js
@@ -0,0 +1,30 @@
+// const config={headers:{Accept:'application/json'}}
+
+// fetch('https://icanhazdadjoke.com/',config)
+// .then(res => {
+//     return res.json();
+// })
+// .then(data => {
+//     console.log(data.joke)
+// })
+
+var n1=document.querySelector('#n1')
+var n2=document.querySelector('#n2')
+var n3=document.querySelector('#n3')
+var c1=document.querySelector('#c1')
+var c2=document.querySelector('#c2')
+var c3=document.querySelector('#c3')
+
+fetch('https://run.mocky.io/v3/f3feef1c-9bfa-43fd-b2a0-bbe9abadb4f6')
+.then(res => {
+    return res.json();
+})
+.then(data => {
+    n1.innerText=data.clients[0].name;
+    n2.innerText=data.clients[1].name;
+    n3.innerText=data.clients[2].name;
+
+    c1.innerText=data.clients[0].company;
+    c2.innerText=data.clients[1].company;
+    c3.innerText=data.clients[2].company;
+})
